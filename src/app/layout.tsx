e3b1f475c52e3d9260bdb9a4f27a461bfadd6f0d
DJ Revell <doug.revelljr@gmail.com>
2025-06-16 21:50:25 +0000

    ok perfect. thank you. Now i have also added 2 images shown in the attac

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/images/dodi-deals-logo-icon.png" sizes="any" />
+        <link rel="apple-touch-icon" href="/images/dodi-deals-mobile-icon.png" />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
         <AppProvider>
